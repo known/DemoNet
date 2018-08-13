fffc8a0f92c66b5208c9c2c058a7b89e30bc667c
known <knownchen@163.com>
2018-08-13 13:50:55 +0800

    Update Menu

--- Demo.WinForm/MainForm.Designer.cs
+++ Demo.WinForm/MainForm.Designer.cs
@@ -28,46 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.MainMenu = new System.Windows.Forms.MenuStrip();
             this.MainStatus = new System.Windows.Forms.StatusStrip();
+            this.TreeMenu = new System.Windows.Forms.TreeView();
             this.PanelContainer = new System.Windows.Forms.Panel();
             this.SuspendLayout();
-            // 
-            // MainMenu
-            // 
-            this.MainMenu.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.MainMenu.Location = new System.Drawing.Point(0, 0);
-            this.MainMenu.Name = "MainMenu";
-            this.MainMenu.Size = new System.Drawing.Size(1006, 24);
-            this.MainMenu.TabIndex = 0;
-            this.MainMenu.Text = "menuStrip1";
             // 
             // MainStatus
             // 
             this.MainStatus.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.MainStatus.Location = new System.Drawing.Point(0, 531);
+            this.MainStatus.Location = new System.Drawing.Point(0, 420);
             this.MainStatus.Name = "MainStatus";
-            this.MainStatus.Size = new System.Drawing.Size(1006, 22);
+            this.MainStatus.Padding = new System.Windows.Forms.Padding(1, 0, 10, 0);
+            this.MainStatus.Size = new System.Drawing.Size(754, 22);
             this.MainStatus.TabIndex = 1;
             this.MainStatus.Text = "statusStrip1";
+            // 
+            // TreeMenu
+            // 
+            this.TreeMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.TreeMenu.Location = new System.Drawing.Point(0, 0);
+            this.TreeMenu.Name = "TreeMenu";
+            this.TreeMenu.Size = new System.Drawing.Size(138, 420);
+            this.TreeMenu.TabIndex = 2;
+            this.TreeMenu.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.TreeMenu_AfterSelect);
             // 
             // PanelContainer
             // 
             this.PanelContainer.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.PanelContainer.Location = new System.Drawing.Point(0, 24);
+            this.PanelContainer.Location = new System.Drawing.Point(138, 0);
             this.PanelContainer.Name = "PanelContainer";
-            this.PanelContainer.Size = new System.Drawing.Size(1006, 507);
-            this.PanelContainer.TabIndex = 2;
+            this.PanelContainer.Padding = new System.Windows.Forms.Padding(5);
+            this.PanelContainer.Size = new System.Drawing.Size(616, 420);
+            this.PanelContainer.TabIndex = 3;
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1006, 553);
+            this.ClientSize = new System.Drawing.Size(754, 442);
             this.Controls.Add(this.PanelContainer);
+            this.Controls.Add(this.TreeMenu);
             this.Controls.Add(this.MainStatus);
-            this.Controls.Add(this.MainMenu);
-            this.MainMenuStrip = this.MainMenu;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "MainForm";
             this.Text = "DemoLite";
             this.Load += new System.EventHandler(this.MainForm_Load);
@@ -77,9 +79,8 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.MenuStrip MainMenu;
         private System.Windows.Forms.StatusStrip MainStatus;
+        private System.Windows.Forms.TreeView TreeMenu;
         private System.Windows.Forms.Panel PanelContainer;
     }
 }
--- Demo.WinForm/MainForm.cs
+++ Demo.WinForm/MainForm.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace Demo.WinForm
 {
@@ -19,6 +14,72 @@ namespace Demo.WinForm
 
         private void MainForm_Load(object sender, EventArgs e)
         {
+            LoadTreeMenu();
+        }
+
+        private void TreeMenu_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (e.Node.Nodes != null && e.Node.Nodes.Count > 0)
+                return;
+
+            PanelContainer.Controls.Clear();
+            Control control = null;
+
+            var typeName = e.Node.Parent != null
+                         ? $"Demo.WinForm.{e.Node.Parent.Name}.{e.Node.Name}Control"
+                         : $"Demo.WinForm.Common.{e.Node.Name}Control";
+            var controlType = Type.GetType(typeName);
+            if (controlType == null)
+                control = CreateErrorControl($"暂无类型控件{typeName}");
+            else
+                control = Activator.CreateInstance(controlType) as Control;
+
+            if (control != null)
+            {
+                control.Dock = DockStyle.Fill;
+                PanelContainer.Controls.Add(control);
+            }
+        }
+
+        private void LoadTreeMenu()
+        {
+            var path = $@"{Application.StartupPath}\Configs\Menu.xml";
+            var doc = new XmlDocument();
+            doc.Load(path);
+            var root = doc.SelectSingleNode("menus");
+
+            foreach (XmlNode node in root.ChildNodes)
+            {
+                var key = node.Attributes["id"].Value;
+                var text = node.Attributes["name"].Value;
+                var treeNode = TreeMenu.Nodes.Add(key, text);
+                LoadTreeMenu(treeNode, node);
+            }
+        }
+
+        private void LoadTreeMenu(TreeNode treeNode, XmlNode xmlNode)
+        {
+            if (!xmlNode.HasChildNodes)
+                return;
+
+            foreach (XmlNode node in xmlNode.ChildNodes)
+            {
+                var key = node.Attributes["id"].Value;
+                var text = node.Attributes["name"].Value;
+                var tn = treeNode.Nodes.Add(key, text);
+                LoadTreeMenu(tn, node);
+            }
+        }
+
+        private Control CreateErrorControl(string message)
+        {
+            var label = new Label();
+            label.Font = new Font(FontFamily.GenericSansSerif, 14);
+            label.ForeColor = Color.Red;
+            label.AutoSize = false;
+            label.TextAlign = ContentAlignment.MiddleCenter;
+            label.Text = message;
+            return label;
         }
     }
 }
