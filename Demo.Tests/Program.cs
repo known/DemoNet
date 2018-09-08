065ca20dcc48cb53d44109c1d282718894f77af0
known <knownchen@163.com>
2018-09-08 22:35:29 +0800

    Test OracleAccess

--- Demo.Tests/Program.cs
+++ Demo.Tests/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
 
 namespace Demo.Tests
 {
@@ -10,7 +11,16 @@ namespace Demo.Tests
     {
         static void Main(string[] args)
         {
+            var connectionString = "Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=XXX)(PORT=1521))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=orcl)));User ID=XXX;Password=XXX;";
+            using (var conn = new OracleConnection(connectionString))
+            {
+                conn.Open();
+                var cmd = new OracleCommand("select code||fullname from t_module where id='1'", conn);
+                var obj = cmd.ExecuteScalar();
+                Console.WriteLine(obj.ToString());
+            }
 
+            Console.ReadKey();
         }
     }
 }
