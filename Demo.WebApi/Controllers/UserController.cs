a56dabb4acb79f2c74b7363da24114184b1c1f9f
known <knownchen@163.com>
2019-01-10 15:49:04 +0800

    Added Swagger

---
+++ Demo.WebApi/Controllers/UserController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Demo.WebApi.Controllers
+{
+    /// <summary>
+    /// 用户管理
+    /// </summary>
+    public class UserController : ApiController
+    {
+        /// <summary>
+        /// 列表
+        /// </summary>
+        /// <returns></returns>
+        // GET: api/User
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        /// <summary>
+        /// 查询
+        /// </summary>
+        /// <param name="id">id</param>
+        /// <returns></returns>
+        // GET: api/User/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        /// <summary>
+        /// 添加
+        /// </summary>
+        /// <param name="value"></param>
+        // POST: api/User
+        public void Post([FromBody]string value)
+        {
+        }
+
+        /// <summary>
+        /// 修改
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="value"></param>
+        // PUT: api/User/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        /// <summary>
+        /// 删除
+        /// </summary>
+        /// <param name="id"></param>
+        // DELETE: api/User/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Demo.WebApi/Global.asax.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web.Http;
+
+namespace Demo.WebApi
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            GlobalConfiguration.Configure(Register);
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        private static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+        }
+    }
+}
